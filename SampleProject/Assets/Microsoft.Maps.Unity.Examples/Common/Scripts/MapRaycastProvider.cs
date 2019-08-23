7ded618f07c2084d3596248550702e0d1021913b
kircher1 <briank@microsoft.com>
2019-08-23 16:02:53 -0700

    Adds a custom raycast provider implementation to the Sample Project. This allows for MRTK's cursors and pointer rays to react to the map. (#20)
    * Adds a custom MRTK raycast provider to the sample project. This custom provider allows for MRTK's input system objects, like cursors and pointer rays, to react to the map's surface geometry.
    
    * Class summaries and cleanup.
    
    * Rename some local variables. Fix typo in comments.

---
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/Common/Scripts/MapRaycastProvider.cs
@@ -0,0 +1,128 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Maps.Unity;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Physics;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Provides an implementation of MRTK's RaycastProvider which allows for MapRenderer surface geometry
+/// to be utilized during ray casts. Only MapRenderers which have been regsitered with this instance
+/// are considered for raycasting, see <see cref="MapRaycastProviderRegistration"/>.
+/// </summary>
+public class MapRaycastProvider : BaseCoreSystem, IMixedRealityRaycastProvider
+{
+    private List<MapRenderer> _mapRenderers = new List<MapRenderer>();
+
+    public MapRaycastProvider(
+        IMixedRealityServiceRegistrar registrar,
+        MixedRealityInputSystemProfile profile) : base(registrar, profile)
+    {
+    }
+
+    public void RegisterMapRenderer(MapRenderer mapRenderer)
+    {
+        if (!_mapRenderers.Contains(mapRenderer))
+        {
+            _mapRenderers.Add(mapRenderer);
+        }
+    }
+
+    public void UnregisterMapRenderer(MapRenderer mapRenderer)
+    {
+        _mapRenderers.Remove(mapRenderer);
+    }
+
+    public bool Raycast(
+        RayStep step,
+        LayerMask[] prioritizedLayerMasks,
+        bool focusIndividualCompoundCollider,
+        out MixedRealityRaycastHit hitInfo)
+    {
+        var hasPhysicsHit =
+            MixedRealityRaycaster.RaycastSimplePhysicsStep(
+                step,
+                prioritizedLayerMasks,
+                focusIndividualCompoundCollider,
+                out RaycastHit physicsHit);
+
+        MapRendererRaycastHit? closestMapHitInfo = null;
+        MapRenderer closestMapRenderer = null;
+        foreach (var mapRenderer in _mapRenderers)
+        {
+            if (
+                mapRenderer.Raycast(
+                    step,
+                    out var mapHitInfo,
+                    hasPhysicsHit ? physicsHit.distance : step.Length))
+            {
+                if (hasPhysicsHit)
+                {
+                    if (physicsHit.distance > mapHitInfo.Distance)
+                    {
+                        if (!closestMapHitInfo.HasValue || closestMapHitInfo.Value.Distance > mapHitInfo.Distance)
+                        {
+                            closestMapRenderer = mapRenderer;
+                            closestMapHitInfo = mapHitInfo;
+                        }
+                    }
+                }
+                else
+                {
+                    if (!closestMapHitInfo.HasValue || closestMapHitInfo.Value.Distance > mapHitInfo.Distance)
+                    {
+                        closestMapRenderer = mapRenderer;
+                        closestMapHitInfo = mapHitInfo;
+                    }
+                }
+            }
+        }
+
+        if (closestMapHitInfo != null)
+        {
+            hitInfo = new MixedRealityRaycastHit();
+            var mapRendererHitInfo = closestMapHitInfo.Value;
+            hitInfo.distance = mapRendererHitInfo.Distance;
+            hitInfo.point = mapRendererHitInfo.Point;
+            hitInfo.normal = mapRendererHitInfo.Normal;
+            hitInfo.transform = closestMapRenderer.transform;
+            return true;
+        }
+        else
+        {
+            hitInfo = new MixedRealityRaycastHit(hasPhysicsHit, physicsHit);
+            return hasPhysicsHit;
+        }
+    }
+
+    public bool SphereCast(
+        RayStep step,
+        float radius,
+        LayerMask[] prioritizedLayerMasks,
+        bool focusIndividualCompoundCollider,
+        out MixedRealityRaycastHit hitInfo)
+    {
+        // For now, this is just using the default behavior for sphere cast.
+        // Leaving MapRenderer integration for a future change.
+
+        var result =
+            MixedRealityRaycaster.RaycastSpherePhysicsStep(
+                step,
+                radius,
+                step.Length,
+                prioritizedLayerMasks,
+                focusIndividualCompoundCollider,
+                out RaycastHit physicsHit);
+        hitInfo = new MixedRealityRaycastHit(result, physicsHit);
+        return result;
+    }
+
+    public RaycastResult GraphicsRaycast(EventSystem eventSystem, PointerEventData pointerEventData, LayerMask[] layerMasks)
+    {
+        return eventSystem.Raycast(pointerEventData, layerMasks);
+    }
+}
---
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/Common/Scripts/MapRaycastProviderRegistration.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Maps.Unity;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+/// <summary>
+/// Registers the MapRenderer component with the MapRaycastProvider.
+/// </summary>
+[DisallowMultipleComponent]
+[RequireComponent(typeof(MapRenderer))]
+public class MapRaycastProviderRegistration : MonoBehaviour
+{
+    private MapRenderer _mapRenderer = null;
+    private MapRaycastProvider _mapRaycastProvider = null;
+
+    private void Start()
+    {
+        _mapRenderer = GetComponent<MapRenderer>();
+        if (
+            _mapRenderer != null &&
+            CoreServices.InputSystem.RaycastProvider is MapRaycastProvider _mapRaycastProvider)
+        {
+            _mapRaycastProvider.RegisterMapRenderer(_mapRenderer);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_mapRaycastProvider != null)
+        {
+            _mapRaycastProvider.UnregisterMapRenderer(_mapRenderer);
+        }
+    }
+}
