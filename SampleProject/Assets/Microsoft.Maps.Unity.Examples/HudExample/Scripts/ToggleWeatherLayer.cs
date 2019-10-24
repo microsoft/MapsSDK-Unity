6049dec05cf8f9935a4e39d9ce2e3e1d414b3fae
Brian Kircher <briank@microsoft.com>
2019-10-23 17:30:16 -0700

    Adds a sample scene to demonstrate new TextureTileLayer. Modifies exsting MapPinExampleScene to demonstrate the MapContourLineLayer.

---
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/HudExample/Scripts/ToggleWeatherLayer.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using UnityEngine;
+
+public class ToggleWeatherLayer : MonoBehaviour
+{
+    public void Toggle()
+    {
+        var existingLayer = GetComponent<WeatherTextureTileLayer>();
+        if (existingLayer == null)
+        {
+            gameObject.AddComponent<WeatherTextureTileLayer>();
+        }
+        else
+        {
+            Destroy(existingLayer);
+        }
+    }
+}
