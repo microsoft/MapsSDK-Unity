d5ca1f79090a98e2e9b109e964998f96253ba110
Brian Kircher <briank@microsoft.com>
2019-11-16 15:22:36 -0800

    Add option to toggle between symbolic and aerial imagery in HudExample.scene.

---
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/HudExample/Scripts/ToggleImageryType.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Maps.Unity;
+using UnityEngine;
+
+public class ToggleImageryType : MonoBehaviour
+{
+    public void Toggle()
+    {
+        var defaultTextureTileLayer = GetComponent<DefaultTextureTileLayer>();
+        if (defaultTextureTileLayer != null)
+        {
+            if (defaultTextureTileLayer.ImageryType == MapImageryType.Aerial)
+            {
+                defaultTextureTileLayer.ImageryType = MapImageryType.Symbolic;
+            }
+            else
+            {
+                defaultTextureTileLayer.ImageryType = MapImageryType.Aerial;
+            }
+        }
+    }
+}
