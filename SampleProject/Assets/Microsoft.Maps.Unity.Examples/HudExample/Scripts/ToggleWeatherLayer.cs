9f1ad9ce8a85faa53b904ae82b35e4539fe60528
Brian Kircher <briank@microsoft.com>
2020-02-26 14:16:08 -0800

    Update project to Unity 2018.4.17f1 and MRTK 2.3.

--- SampleProject/Assets/Microsoft.Maps.Unity.Examples/HudExample/Scripts/ToggleWeatherLayer.cs
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/HudExample/Scripts/ToggleWeatherLayer.cs
@@ -1,20 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Maps.Unity;
 using UnityEngine;
 
 public class ToggleWeatherLayer : MonoBehaviour
 {
     public void Toggle()
     {
-        var existingLayer = GetComponent<WeatherTextureTileLayer>();
-        if (existingLayer == null)
-        {
-            gameObject.AddComponent<WeatherTextureTileLayer>();
-        }
-        else
-        {
-            Destroy(existingLayer);
-        }
+        var existingLayer = GetComponent<HttpTextureTileLayer>();
+        existingLayer.enabled = !existingLayer.enabled;
     }
 }
