5e2c159c4c8ec846304984f0f9032be75556751c
Brian Kircher <briank@microsoft.com>
2020-06-19 10:22:19 -0700

    Update MapPinExample.

--- SampleProject/Assets/Microsoft.Maps.Unity.Examples/Common/Scripts/TransferLabelText.cs
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/Common/Scripts/TransferLabelText.cs
@@ -13,10 +13,34 @@ public class TransferLabelText : MonoBehaviour
     [SerializeField]
     MapLabel _mapLabel = null;
 
-    void Start()
+    private void Start()
     {
         Debug.Assert(_textMeshPro != null);
         Debug.Assert(_mapLabel != null);
+
         _textMeshPro.text = _mapLabel.Text;
+        _textMeshPro.color = _mapLabel.Style.Color;
+        _textMeshPro.fontStyle = ConvertFontStyle(_mapLabel.Style);
+    }
+
+    private FontStyles ConvertFontStyle(Style mapLabelStyle)
+    {
+        FontStyles result = 0;
+
+        switch (mapLabelStyle.FontStyle)
+        {
+            case Microsoft.Maps.Unity.FontStyle.Italic:
+                result |= FontStyles.Italic;
+                break;
+        }
+
+        switch (mapLabelStyle.FontWeight)
+        {
+            case Microsoft.Maps.Unity.FontWeight.Bold:
+                result |= FontStyles.Bold;
+                break;
+        }
+
+        return result;
     }
 }
